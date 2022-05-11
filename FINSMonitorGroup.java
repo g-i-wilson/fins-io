3eb03ec67ef6589c69c48e34a36192d984d4c0ee
Gabriel Wilson <47883480+g-i-wilson@users.noreply.github.com>
2022-05-11 10:12:41 -0500

    started work on FINSMonitorGroup.java

--- FINSMonitorGroup.java
+++ FINSMonitorGroup.java
@@ -1,51 +1,42 @@
 import java.util.*;
 import paddle.*;
+import creek.*;
 
 public class FINSMonitorGroup {
-
+	
+	private CSVFile machines;
 	private List<Timer> timers;
 	private Map<String,Map<String,String>> memoryMaps;
 
 
 	public FINSMonitorGroup (
+		String machinesFilePath,
 		String localFinsAddress,
 		String dbAddress,
 		int dbPort,
-		String machinesPath,
 		String eventsPath
-	) throws Exception {
+	) throws Exception
+	{
+		machines = new CSVFile( machinesFilePath );
 		timers = new ArrayList<>();
 		memoryMaps = new HashMap<>();
+					
+		for (List<String> machineData : machines.rows()) {
 		
-		String machinesStr =
-			(new OutboundTCP( dbAddress, dbPort, "GET /"+machinesPath+" HTTP/1.1\r\n\r\n" ))
-			.receive()
-			.text();
-			
-		//System.out.println( machinesStr );
-		
-		String[] machines = machinesStr.split("\n");
-		
-		//for (String s : machines ) {
-		//	System.out.println( "line: "+s );
-		//}
-			
-		for (int i=4; i<machines.length-1; i++) {
-		
-			//System.out.println( "processing line: "+machines[i] );
-			String[] machineData = machines[i].split(",");
 			Map memoryMap = new HashMap<String,String>();
-			memoryMaps.put( machineData[0], memoryMap );
+			memoryMaps.put( machineData.get(0), memoryMap );
 			
-			if (machineData.length > 8) {
+			
+			
+			try {
 				FINSMonitor fm = new FINSMonitor(
 					localFinsAddress,
 					
-					machineData[0],
-					Integer.parseInt(machineData[1]),
-					machineData[2],
+					machineData.get(0),
+					Integer.parseInt(machineData.get(1)),
+					machineData.get(2),
 					
-					Arrays.copyOfRange(machineData, 5, machineData.length-1),
+					String.split( machineData.get(5), "," ),
 					memoryMap,
 					
 					3000,
@@ -59,8 +50,12 @@ public class FINSMonitorGroup {
 				Timer t = new Timer();
 				t.scheduleAtFixedRate( fm, 0, 3000 );
 				timers.add( t );
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
+			
 		}
+	
 	}
 	
 	public String toString () {
